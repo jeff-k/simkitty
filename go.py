320e49fb0a598380e06cbb9613bdc098b54b57be
jeff-k <jeff_k@fastmail.com>
2020-11-14 12:31:22 +0000

    expose user controls for all joints, slightly less nonsensical initial values

--- go.py
+++ go.py
@@ -13,12 +13,21 @@ planeId = p.loadURDF("plane.urdf")
 
 orientation = p.getQuaternionFromEuler([0,0,0])
 
-#knee = p.addUserDebugParameter('Knee', -0.7, 0.7, 0)
 
 cat = Opencat(p, (0, 0, 1), orientation)
 
+joint_controls = {}
+for joint in cat.joints:
+    jn = '-'.join(joint)
+    print(jn)
+    joint_controls[jn] = p.addUserDebugParameter(jn, -0.7, 0.7, 0)
+
+print("joints")
+print(cat.joints)
+
 while True:
-#    cat.setLeftLeg(p.readUserDebugParameter(knee))
+    for joint in joint_controls:
+        cat.set_joint(joint, p.readUserDebugParameter(joint_controls[joint]))
     p.stepSimulation()
 
 print("done")
